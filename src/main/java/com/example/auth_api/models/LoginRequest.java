5dd42367336562d1489ba315bd63ee0c327d7f4a
Sergey Kurochkin <serega.curo4kin2014@@gmail.com>
2025-03-05 21:08:28 +0300

    removed unused annotations from LoginRequest.java and RegistrationRequest.java

--- src/main/java/com/example/auth_api/models/LoginRequest.java
+++ src/main/java/com/example/auth_api/models/LoginRequest.java
@@ -1,7 +1,6 @@
 package com.example.auth_api.models;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+
 import lombok.*;
 import org.springframework.validation.annotation.Validated;
 
@@ -12,8 +11,6 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 public class LoginRequest {
 
-    @NonNull
     private String username;
-    @NotBlank(message = "Password must not be blank")
     private String password;
 }
--- src/main/java/com/example/auth_api/models/RegistrationRequest.java
+++ src/main/java/com/example/auth_api/models/RegistrationRequest.java
@@ -1,6 +1,5 @@
 package com.example.auth_api.models;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
